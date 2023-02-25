da7f4a7a4ae75cf5eb9e05f9220985ce7a9ca72c
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-25 22:24:15 +0530

    basic css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       
     </div>
   );
