d78f93ff2f4df448d9caa3d4be82deba977ea363
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-05 09:29:51 +0530

    product page styling and effects

--- src/components/Product.js
+++ src/components/Product.js
@@ -12,12 +12,13 @@ export default class Product extends Component {
         <div className='card'>
           <div className='img-container p-5' onClick={(event) => {
             event.stopPropagation()
-            console.log("you click me from imag container")}}>
+            console.log("you click me from imag container")
+          }}>
             <Link to="/details">
               <img src={img} alt="product" className='card-img-top' />
             </Link>
-            <button onClick={()=>{console.log("added to the cart")}} className='cart-btn' type='button' disabled={inCart ? true : false}>
-              {inCart?<p className='text-capitalize mb-0' disabled>in cart</p>:<i className='fas fa-cart-plus' />}
+            <button onClick={() => { console.log("added to the cart") }} className='cart-btn' type='button' disabled={inCart ? true : false}>
+              {inCart ? <p className='text-capitalize mb-0' disabled>in cart</p> : <i className='fas fa-cart-plus' />}
             </button>
           </div>
           {/* card footer */}
@@ -35,5 +36,56 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
+.card {
+  border-color:transparent;
+  transition: all 1s linear;
+}
+.card-footer {
+  background: transparent;
+  border-top: transparent;
+  transition: all 1s linear;
+}
+&:hover{
+  .card {
+    border:0.04rem solid rgba(0,0,0,0.2);
+    box-shadow: 2px 2px 5px 0 rgba(0,0,0,0.2);
+  }
+  .card-footer {
+    background: rgba(200,200,200);
+  }
+}
+.img-container {
+  position:relative;
+  overflow:hidden;
+}
 
+.card-img-top {
+  transition: all 1s linear;
+}
+.img-container:hover .card-img-top {
+  transform: scale(1.2);
+}
+
+.cart-btn {
+  position: absolute;
+  bottom:0;
+  right:0;
+  padding:0.2rem 0.4rem;
+  background: var(--lightBlue);
+  border: none;
+  color: var(--mainWhite);
+  font-size:1.4rem;
+  border-radius: 0.5rem 0 0 0 ;
+  transform: translate(100%,100%);
+  transition: all 1s linear;
+}
+
+.img-container:hover .cart-btn {
+  transform: translate(0,0)
+}
+
+.cart-btn:hover {
+  color: var(--mainBlue);
+  cursor: pointer;
+}
 `
