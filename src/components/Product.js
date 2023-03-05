5e7abcd5cf16b5e05fb9e344a06ad5b7281323b7
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-05 09:36:10 +0530

    product page indentation fixed

--- src/components/Product.js
+++ src/components/Product.js
@@ -36,56 +36,60 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
-.card {
-  border-color:transparent;
-  transition: all 1s linear;
-}
-.card-footer {
-  background: transparent;
-  border-top: transparent;
-  transition: all 1s linear;
-}
-&:hover{
   .card {
-    border:0.04rem solid rgba(0,0,0,0.2);
-    box-shadow: 2px 2px 5px 0 rgba(0,0,0,0.2);
+    border-color:transparent;
+    transition: all 1s linear;
   }
+
   .card-footer {
-    background: rgba(200,200,200);
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
   }
-}
-.img-container {
-  position:relative;
-  overflow:hidden;
-}
 
-.card-img-top {
-  transition: all 1s linear;
-}
-.img-container:hover .card-img-top {
-  transform: scale(1.2);
-}
+  &:hover{
+    .card {
+      border:0.04rem solid rgba(0,0,0,0.2);
+      box-shadow: 2px 2px 5px 0 rgba(0,0,0,0.2);
+    }
 
-.cart-btn {
-  position: absolute;
-  bottom:0;
-  right:0;
-  padding:0.2rem 0.4rem;
-  background: var(--lightBlue);
-  border: none;
-  color: var(--mainWhite);
-  font-size:1.4rem;
-  border-radius: 0.5rem 0 0 0 ;
-  transform: translate(100%,100%);
-  transition: all 1s linear;
-}
+    .card-footer {
+      background: rgba(200,200,200);
+    }
+  }
+  .img-container {
+    position:relative;
+    overflow:hidden;
+  }
 
-.img-container:hover .cart-btn {
-  transform: translate(0,0)
-}
+  .card-img-top {
+    transition: all 1s linear;
+  }
 
-.cart-btn:hover {
-  color: var(--mainBlue);
-  cursor: pointer;
-}
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+
+  .cart-btn {
+    position: absolute;
+    bottom:0;
+    right:0;
+    padding:0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size:1.4rem;
+    border-radius: 0.5rem 0 0 0 ;
+    transform: translate(100%,100%);
+    transition: all 1s linear;
+  }
+
+  .img-container:hover .cart-btn {
+    transform: translate(0,0)
+  }
+
+  .cart-btn:hover {
+    color: var(--mainBlue);
+    cursor: pointer;
+  }
 `
