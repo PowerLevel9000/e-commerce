b0dbb9e3444ba8da918dcbb51ad216789babba4b
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-05 11:01:47 +0530

    refferece change to the copy in context

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { ProductConsumer } from '../context'
+import { PropTypes } from "prop-types"
 
 
 export default class Product extends Component {
@@ -34,6 +35,16 @@ export default class Product extends Component {
     )
   }
 }
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool,
+  }).isRequired
+};
 
 const ProductWrapper = styled.div`
   .card {
--- src/context.js
+++ src/context.js
@@ -9,16 +9,34 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct,
   }
+
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () =>{
+    let tempProducts = [];
+    storeProducts.forEach(item=>{
+      const singleItem = {...item };
+      tempProducts = [...tempProducts,singleItem];
+    })
+    this.setState(()=>{
+      return {products:tempProducts}
+    })
+  }
+
   handleDetail = (event) => {
     event.stopPropagation()
     console.log("Hello from detials")
   }
+
   addToCart = () => {
     console.log("Hello from add to cart")
   }
+  
   render() {
     return (
       <ProductContext.Provider value={{
