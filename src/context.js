783577dbe59d4bd67637bb3dad0450c4665fda6d
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-06 11:33:32 +0530

    remove item funtionality

--- src/context.js
+++ src/context.js
@@ -83,7 +83,25 @@ class ProductProvider extends Component {
   }
 
   removeItem = (id) => {
-    console.log("item removed : ", id)
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !==id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct =  tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(()=>{
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+      };
+    },()=>{
+      this.addTotal();
+    });
+
   }
 
   clearCart = (id) => {
