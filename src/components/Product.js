0ab49809c2a93794b735145af8771e1fac8d6e14
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-12 09:15:41 +0530

    loader only when img not loaded

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,56 +10,54 @@ export default class Product extends Component {
     loading: true
   };
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ loading: false });
-    }, 500);
-  }
+  handleImageLoaded = () => {
+    this.setState({ loading: false });
+  };
 
   render() {
     const { id, title, img, price, inCart } = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          {this.state.loading ? (
+          {this.state.loading && (
             <CirclesWithBar color="rgba(56, 160, 186, 0.8)" height="300" width="250"/>
-          ) : (
-            <ProductConsumer>
-              {(value) => (
-                <div
-                  className="img-container p-5"
-                  onClick={() => {
-                    value.handleDetail(id);
-                  }}
-                >
-                  <Link to="/details">
-                    <img
-                      src={img}
-                      alt="product"
-                      className="card-img-top"
-                    />
-                  </Link>
-                  <button
-                    onClick={() => {
-                      value.addToCart(id);
-                      value.openModal(id);
-                    }}
-                    className="cart-btn"
-                    type="button"
-                    disabled={inCart ? true : false}
-                  >
-                    {inCart ? (
-                      <p className="text-capitalize mb-0" disabled>
-                        in cart
-                      </p>
-                    ) : (
-                      <i className="fas fa-cart-plus" />
-                    )}
-                  </button>
-                </div>
-              )}
-            </ProductConsumer>
           )}
+          <ProductConsumer>
+            {(value) => (
+              <div
+                className="img-container p-5"
+                onClick={() => {
+                  value.handleDetail(id);
+                }}
+              >
+                <Link to="/details">
+                  <img
+                    src={img}
+                    alt="product"
+                    className="card-img-top"
+                    onLoad={this.handleImageLoaded}
+                  />
+                </Link>
+                <button
+                  onClick={() => {
+                    value.addToCart(id);
+                    value.openModal(id);
+                  }}
+                  className="cart-btn"
+                  type="button"
+                  disabled={inCart ? true : false}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      in cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
           {/* card footer */}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
@@ -73,6 +71,7 @@ export default class Product extends Component {
     );
   }
 }
+
 
 Product.propTypes = {
   product: PropTypes.shape({
